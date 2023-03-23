2039cd8569cbb8e31bccc3af7e731f0c7ded4bfd
zizudana <zizudana@naver.com>
2023-03-23 17:28:14 +0900

    study: 입출력,자료형

---
+++ 230323/230323-1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main()
+{
+    int a = 1;
+    int b = 2;
+    std::cout << a+b << "\n";
+
+    float f = 1.2;
+    float g = 3.9;
+    std::cout << f+g << "\n";
+
+    bool c1 = true;
+    std::cout << c1 << "\n";
+
+    bool c2 = false;
+    std::cout << c2 << "\n";
+
+    char d = 'a';
+    std::cout << d << "\n";
+
+    char e = 'b';
+    std::cout << e << "\n";
+
+    std::string str1 = "안녕?";
+    std::cout << str1 << "\n";
+}
