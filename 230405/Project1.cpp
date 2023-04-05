929a13bcd33971e3e3e1a51a04c4862e002f41cf
zizudana <zizudana@naver.com>
2023-04-05 18:19:24 +0900

    Edit: 중복되는 부분을 함수로 처리

--- 230405/Project1.cpp
+++ 230405/Project1.cpp
@@ -4,43 +4,45 @@
 #include <ctime>
 using namespace std;
 
+int total = 1;
+
+int baskin_game(int num, int who) //who : 사용자 1 컴퓨터 0
+{
+    for (int i=0;i<num;i++)
+    {
+        cout << total++ << endl;
+        if(total > 31)
+        {
+            if (who == 1)
+                cout << "게임 종료! 컴퓨터의 승리입니다." << endl;
+            else
+                cout << "게임 종료! 사용자의 승리입니다." << endl;
+            return 1;
+        }
+    }
+    return 0;
+}
+
 int main()
 {
-    int user_num; //사용자가 입력한 숫자
-    int com_num; //컴퓨터가 부를 숫자
+    int num; //부를 개수
     int total = 1; //현재까지 부른 수
     int i;
     srand(time(NULL));
-    
+
     while(1)
     {
         cout << "\n개수를 입력하세요: ";
-        cin >> user_num;
-        if (user_num < 1 | user_num >3)
+        cin >> num;
+        if (num < 1 | num >3)
         {
             cout << "1~3 사이의 수를 입력하세요" << endl;
             continue;
         }
         cout << "사용자가 부른 숫자!" << endl;
-        for (i=0;i<user_num;i++)
-        {
-            cout << total++ << endl;
-            if(total > 31)
-            {
-                cout << "게임 종료! 컴퓨터의 승리입니다." << endl;
-                return 0;
-            }
-        }
+        if (baskin_game(num, 1)==1) return 0;
         cout << "\n컴퓨터가 부른 숫자!" << endl;
-        com_num = rand()%3 + 1;
-        for (i=0;i<com_num;i++)
-        {
-            cout << total++ << endl;
-            if(total > 31)
-            {
-                cout << "게임 종료! 사용자의 승리입니다." << endl;
-                return 0;
-            }
-        }
+        num = rand()%3 + 1;
+        if (baskin_game(num, 0)==1) return 0;
     }
 }
